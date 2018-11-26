4084b133be7a6af8ff36ec7a5de0761c1f9f0a14
Pavan Jadda <jpavanaryan@gmail.com>
2018-11-26 12:09:53 -0500

    First Commit

---
+++ src/main/java/com/networking/AccessRemoteHostFiles.java
@@ -0,0 +1,14 @@
+package com.networking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessRemoteHostFiles
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AccessRemoteHostFiles.class, args);
+    }
+}
---
+++ src/test/java/com/networking/AccessRemoteHostFilesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.networking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessRemoteHostFilesApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
