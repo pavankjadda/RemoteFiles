4b0f8a23f389dc62e258b5f3286c5320d21b4ed3
Pavan Jadda <pjadda@bcmcgroup.com>
2018-11-27 14:30:55 -0500

    Added remote_hosts.json files

--- src/main/java/com/networking/home/GetRemoteFiles.java
+++ src/main/java/com/networking/home/GetRemoteFiles.java
@@ -8,12 +8,12 @@ public class GetRemoteFiles
     public static void main(String[] args)
     {
         DownloadOperations downloadOperations= new DownloadOperations();
-
         //downloadOperations.copyFilesFromDirectory("/home/cuckoo/.cuckoo/storage/analyses/","/home/cuckoo/Desktop/MalwareReports/");
         //downloadOperations.listFilesInDirectory("/home/cuckoo/.cuckoo/storage/analyses/");
 
-
         DeleteAnalyzedMalwareFiles deleteAnalyzedMalwareFiles=new DeleteAnalyzedMalwareFiles();
         deleteAnalyzedMalwareFiles.deleteAnalyzedFiles("/home/cuckoo/Desktop/VirusShare/VirusShare_00000/","/home/cuckoo/.cuckoo/storage/analyses/");
+
+
     }
 }
