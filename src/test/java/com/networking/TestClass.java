c905257422292fdcf41bb17dd297cf98e28b4fb8
Pavan Jadda <pjadda@bcmcgroup.com>
2018-12-03 18:19:58 -0500

    Multi Threading Support added

--- src/test/java/com/networking/TestClass.java
+++ src/test/java/com/networking/TestClass.java
@@ -1,18 +1,58 @@
 package com.networking;
 
 import javax.net.ssl.SSLSocketFactory;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestClass
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        File[] listFiles=new File("/media/cuckoo/VirusShare/Malware_JSON_Reports/").listFiles();
+        assert listFiles != null;
+        for(File fileEntry:listFiles)
+        {
+            if (!fileEntry.isDirectory() && !fileEntry.getName().equals("malwares_json_reports_1.tar.xz"))
+            {
+                String fileName=fileEntry.getName();
+                String newFileName=changeName(fileName);
+
+                Files.move(Paths.get(fileEntry.getAbsolutePath()), Paths.get("/media/cuckoo/VirusShare/Malware_JSON_Reports/malwares/"+newFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+
         //RemoteOperationsUtil remoteOperationsUtil=new RemoteOperationsUtil();
         //remoteOperationsUtil.moveFiles("/home/cuckoo/Desktop/MalwareReports","/media/cuckoo/VirusShare/Malware_JSON_Reports/malwares/");
     }
+
+    private static String changeName(String fileName)
+    {
+        String pattern = "(?<=malwares)[0-9a-zA-Z-]+.json";
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        // Now create matcher object.
+        Matcher m = r.matcher(fileName);
+        if (m.find())
+        {
+            System.out.println("Found value: " + m.group(0));
+        }
+        else
+        {
+            System.out.println("NO MATCH");
+        }
+        return m.group(0);
+    }
+
 
     static void test() throws IOException
     {
